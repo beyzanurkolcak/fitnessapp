4cdb0439f767eaacea2617304fa33e93d0931ac1
Beyzanur Kolçak <165806344+beyzanurkolcak@users.noreply.github.com>
2024-12-12 23:55:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace fitness2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.YasTextBox = new System.Windows.Forms.TextBox();
+            this.BoyTextBox = new System.Windows.Forms.TextBox();
+            this.KiloTextBox = new System.Windows.Forms.TextBox();
+            this.HedefComboBox = new System.Windows.Forms.ComboBox();
+            this.HareketSeviyesiComboBox = new System.Windows.Forms.ComboBox();
+            this.Gun6TextBox = new System.Windows.Forms.TextBox();
+            this.Gun5TextBox = new System.Windows.Forms.TextBox();
+            this.Gun3TextBox = new System.Windows.Forms.TextBox();
+            this.Gun2TextBox = new System.Windows.Forms.TextBox();
+            this.Gun7TextBox = new System.Windows.Forms.TextBox();
+            this.Gun4TextBox = new System.Windows.Forms.TextBox();
+            this.Gun1TextBox = new System.Windows.Forms.TextBox();
+            this.HaftalikDegisimDataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.HaftalikDegisimKaydetButton = new System.Windows.Forms.Button();
+            this.KullaniciIDTextBox = new System.Windows.Forms.TextBox();
+            this.fitnessDataSet = new fitness2.fitnessDataSet();
+            this.fitnessDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.HaftalikDegisimDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fitnessDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fitnessDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // YasTextBox
+            // 
+            this.YasTextBox.Location = new System.Drawing.Point(74, 36);
+            this.YasTextBox.Name = "YasTextBox";
+            this.YasTextBox.Size = new System.Drawing.Size(100, 22);
+            this.YasTextBox.TabIndex = 0;
+            // 
+            // BoyTextBox
+            // 
+            this.BoyTextBox.Location = new System.Drawing.Point(74, 83);
+            this.BoyTextBox.Name = "BoyTextBox";
+            this.BoyTextBox.Size = new System.Drawing.Size(100, 22);
+            this.BoyTextBox.TabIndex = 1;
+            // 
+            // KiloTextBox
+            // 
+            this.KiloTextBox.Location = new System.Drawing.Point(74, 130);
+            this.KiloTextBox.Name = "KiloTextBox";
+            this.KiloTextBox.Size = new System.Drawing.Size(100, 22);
+            this.KiloTextBox.TabIndex = 2;
+            // 
+            // HedefComboBox
+            // 
+            this.HedefComboBox.FormattingEnabled = true;
+            this.HedefComboBox.Location = new System.Drawing.Point(218, 51);
+            this.HedefComboBox.Name = "HedefComboBox";
+            this.HedefComboBox.Size = new System.Drawing.Size(121, 24);
+            this.HedefComboBox.TabIndex = 4;
+            // 
+            // HareketSeviyesiComboBox
+            // 
+            this.HareketSeviyesiComboBox.FormattingEnabled = true;
+            this.HareketSeviyesiComboBox.Location = new System.Drawing.Point(218, 128);
+            this.HareketSeviyesiComboBox.Name = "HareketSeviyesiComboBox";
+            this.HareketSeviyesiComboBox.Size = new System.Drawing.Size(121, 24);
+            this.HareketSeviyesiComboBox.TabIndex = 5;
+            // 
+            // Gun6TextBox
+            // 
+            this.Gun6TextBox.Location = new System.Drawing.Point(581, 221);
+            this.Gun6TextBox.Name = "Gun6TextBox";
+            this.Gun6TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Gun6TextBox.TabIndex = 9;
+            // 
+            // Gun5TextBox
+            // 
+            this.Gun5TextBox.Location = new System.Drawing.Point(581, 180);
+            this.Gun5TextBox.Name = "Gun5TextBox";
+            this.Gun5TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Gun5TextBox.TabIndex = 8;
+            // 
+            // Gun3TextBox
+            // 
+            this.Gun3TextBox.Location = new System.Drawing.Point(581, 117);
+            this.Gun3TextBox.Name = "Gun3TextBox";
+            this.Gun3TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Gun3TextBox.TabIndex = 7;
+            // 
+            // Gun2TextBox
+            // 
+            this.Gun2TextBox.Location = new System.Drawing.Point(581, 83);
+            this.Gun2TextBox.Name = "Gun2TextBox";
+            this.Gun2TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Gun2TextBox.TabIndex = 6;
+            // 
+            // Gun7TextBox
+            // 
+            this.Gun7TextBox.Location = new System.Drawing.Point(581, 258);
+            this.Gun7TextBox.Name = "Gun7TextBox";
+            this.Gun7TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Gun7TextBox.TabIndex = 12;
+            // 
+            // Gun4TextBox
+            // 
+            this.Gun4TextBox.Location = new System.Drawing.Point(581, 152);
+            this.Gun4TextBox.Name = "Gun4TextBox";
+            this.Gun4TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Gun4TextBox.TabIndex = 11;
+            // 
+            // Gun1TextBox
+            // 
+            this.Gun1TextBox.Location = new System.Drawing.Point(581, 51);
+            this.Gun1TextBox.Name = "Gun1TextBox";
+            this.Gun1TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Gun1TextBox.TabIndex = 10;
+            // 
+            // HaftalikDegisimDataGridView
+            // 
+            this.HaftalikDegisimDataGridView.AutoGenerateColumns = false;
+            this.HaftalikDegisimDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.HaftalikDegisimDataGridView.DataSource = this.fitnessDataSetBindingSource;
+            this.HaftalikDegisimDataGridView.Location = new System.Drawing.Point(115, 270);
+            this.HaftalikDegisimDataGridView.Name = "HaftalikDegisimDataGridView";
+            this.HaftalikDegisimDataGridView.RowHeadersWidth = 51;
+            this.HaftalikDegisimDataGridView.RowTemplate.Height = 24;
+            this.HaftalikDegisimDataGridView.Size = new System.Drawing.Size(240, 150);
+            this.HaftalikDegisimDataGridView.TabIndex = 13;
+            this.HaftalikDegisimDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "yaş";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "boy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "kilo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(299, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Fitness Uygulaması";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(271, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "hedef";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(261, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "hareket";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(218, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(517, 58);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "1. Gün";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(520, 88);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 16);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "2. Gün";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(517, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 16);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "3.Gün";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(517, 155);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 16);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "4.Gün";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(523, 183);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 16);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "5.Gün";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(523, 221);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 16);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "6.Gün";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(520, 261);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 16);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "7.Gün";
+            // 
+            // HaftalikDegisimKaydetButton
+            // 
+            this.HaftalikDegisimKaydetButton.Location = new System.Drawing.Point(713, 168);
+            this.HaftalikDegisimKaydetButton.Name = "HaftalikDegisimKaydetButton";
+            this.HaftalikDegisimKaydetButton.Size = new System.Drawing.Size(75, 23);
+            this.HaftalikDegisimKaydetButton.TabIndex = 28;
+            this.HaftalikDegisimKaydetButton.Text = "Kaydet";
+            this.HaftalikDegisimKaydetButton.UseVisualStyleBackColor = true;
+            // 
+            // KullaniciIDTextBox
+            // 
+            this.KullaniciIDTextBox.Location = new System.Drawing.Point(74, 185);
+            this.KullaniciIDTextBox.Name = "KullaniciIDTextBox";
+            this.KullaniciIDTextBox.Size = new System.Drawing.Size(100, 22);
+            this.KullaniciIDTextBox.TabIndex = 32;
+            // 
+            // fitnessDataSet
+            // 
+            this.fitnessDataSet.DataSetName = "fitnessDataSet";
+            this.fitnessDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // fitnessDataSetBindingSource
+            // 
+            this.fitnessDataSetBindingSource.DataSource = this.fitnessDataSet;
+            this.fitnessDataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.KullaniciIDTextBox);
+            this.Controls.Add(this.HaftalikDegisimKaydetButton);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HaftalikDegisimDataGridView);
+            this.Controls.Add(this.Gun7TextBox);
+            this.Controls.Add(this.Gun4TextBox);
+            this.Controls.Add(this.Gun1TextBox);
+            this.Controls.Add(this.Gun6TextBox);
+            this.Controls.Add(this.Gun5TextBox);
+            this.Controls.Add(this.Gun3TextBox);
+            this.Controls.Add(this.Gun2TextBox);
+            this.Controls.Add(this.HareketSeviyesiComboBox);
+            this.Controls.Add(this.HedefComboBox);
+            this.Controls.Add(this.KiloTextBox);
+            this.Controls.Add(this.BoyTextBox);
+            this.Controls.Add(this.YasTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.HaftalikDegisimDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fitnessDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fitnessDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox YasTextBox;
+        private System.Windows.Forms.TextBox BoyTextBox;
+        private System.Windows.Forms.TextBox KiloTextBox;
+        private System.Windows.Forms.ComboBox HedefComboBox;
+        private System.Windows.Forms.ComboBox HareketSeviyesiComboBox;
+        private System.Windows.Forms.TextBox Gun6TextBox;
+        private System.Windows.Forms.TextBox Gun5TextBox;
+        private System.Windows.Forms.TextBox Gun3TextBox;
+        private System.Windows.Forms.TextBox Gun2TextBox;
+        private System.Windows.Forms.TextBox Gun7TextBox;
+        private System.Windows.Forms.TextBox Gun4TextBox;
+        private System.Windows.Forms.TextBox Gun1TextBox;
+        private System.Windows.Forms.DataGridView HaftalikDegisimDataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button HaftalikDegisimKaydetButton;
+        private System.Windows.Forms.TextBox KullaniciIDTextBox;
+        private System.Windows.Forms.BindingSource fitnessDataSetBindingSource;
+        private fitnessDataSet fitnessDataSet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace fitness2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        string connectionString = @"Server=localhost;Database=FitnessDB;Trusted_Connection=True;";
+
+        // Kullanıcı bilgilerini veritabanına kaydetme
+        private void KaydetButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Kullanici (Yas, Boy, Kilo, Hedef, HareketSeviyesi) VALUES (@Yas, @Boy, @Kilo, @Hedef, @HareketSeviyesi)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Yas", int.Parse(YasTextBox.Text));
+                    command.Parameters.AddWithValue("@Boy", float.Parse(BoyTextBox.Text));
+                    command.Parameters.AddWithValue("@Kilo", float.Parse(KiloTextBox.Text));
+                    command.Parameters.AddWithValue("@Hedef", HedefComboBox.SelectedItem.ToString());
+                    command.Parameters.AddWithValue("@HareketSeviyesi", HareketSeviyesiComboBox.SelectedItem.ToString());
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Kullanıcı bilgileri başarıyla kaydedildi.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        // Haftalık değişimleri veritabanına kaydetme
+        private void HaftalikDegisimKaydetButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO HaftalikDegisim (KullaniciID, Gun1, Gun2, Gun3, Gun4, Gun5, Gun6, Gun7) " +
+                                   "VALUES (@KullaniciID, @Gun1, @Gun2, @Gun3, @Gun4, @Gun5, @Gun6, @Gun7)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@KullaniciID", int.Parse(KullaniciIDTextBox.Text)); // Kullanıcı ID'sini al
+                    command.Parameters.AddWithValue("@Gun1", float.Parse(Gun1TextBox.Text));
+                    command.Parameters.AddWithValue("@Gun2", float.Parse(Gun2TextBox.Text));
+                    command.Parameters.AddWithValue("@Gun3", float.Parse(Gun3TextBox.Text));
+                    command.Parameters.AddWithValue("@Gun4", float.Parse(Gun4TextBox.Text));
+                    command.Parameters.AddWithValue("@Gun5", float.Parse(Gun5TextBox.Text));
+                    command.Parameters.AddWithValue("@Gun6", float.Parse(Gun6TextBox.Text));
+                    command.Parameters.AddWithValue("@Gun7", float.Parse(Gun7TextBox.Text));
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Haftalık değişimler başarıyla kaydedildi.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        // Haftalık kilo değişimlerini analiz etme
+        private void HaftalikAnalizButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM HaftalikDegisim WHERE KullaniciID = @KullaniciID";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@KullaniciID", int.Parse(KullaniciIDTextBox.Text));
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    // Haftalık değişimleri DataGridView'de görüntüle
+                    HaftalikDegisimDataGridView.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+    }
+}
+    
